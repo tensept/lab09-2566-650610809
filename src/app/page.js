1ee781e963762884078c9f44c6f34730c3c3fb2c
tensept <vankaew58@gmail.com>
2023-07-20 21:43:45 +0700

    lab09

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,26 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  const [All, setAll] = useState(0);
+  const [Done, setDone] = useState(0);
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
+    setAll(All+1);
   };
 
   const deleteTask = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    const taskToDelete = tasks.find((task) => task.id === taskId);
+    if (taskToDelete.completed) {
+      setDone(Done-1)
+    } else {
+      setDone(Done);
+    }
+    setAll(All - 1);
   };
 
   const toggleDoneTask = (taskId) => {
@@ -30,6 +40,8 @@ export default function Home() {
     const task = newTasks.find((x) => x.id === taskId);
     task.completed = !task.completed;
     setTasks(newTasks);
+    const completedTask = newTasks.filter((task) => task.completed)
+    setDone(completedTask.length)
   };
 
   return (
@@ -41,7 +53,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({All}) Done ({Done})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +72,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Waenkaew Phimsena" studentId="650610809" />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -7,18 +7,19 @@ export const Task = ({
 }) => {
   const deleteBtnOnClick = () => {
     deleteTaskFunc(id);
+    completed = false
   };
+
+  const resultDone = completed ? "text-decoration-line-through" : ""
+  
+  const toggleDoneBtnOnClick = () =>{
+    toggleDoneTaskFunc(id)
+  }
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {/*
-      HINTS: if task is completed, below "span" will show like this 
-        <span className="text-decoration-line-through">{title}</span>
-        But if task is not completed : 
-        <span>{title}</span>
-      */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <span className={resultDone}>{title}</span>
+      <button className="btn btn-success" onClick={toggleDoneBtnOnClick}>Done</button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
